2cf571759a828ebe9798a7cbefe2d385c3ba7157
Borghino Jeremy <pp.btsig@free.fr>
2017-05-02 09:56:15 +0200

    debut receive file

--- src/appli_User_TF.java
+++ src/appli_User_TF.java
@@ -1,8 +1,47 @@
+import java.io.IOException;
 import java.net.*;
 
 public class appli_User_TF 
 {
-	public void ReceiveFile(){
+	
+	private DatagramSocket cli;
+	
+	public int ReceiveFile(int port, InetAddress ip, String nom)
+	{
 		
+		try{
+			DatagramSocket cli = new DatagramSocket();
+			// suite du programme
+		}
+		catch (SocketException ex) 
+		{
+			System.err.println("Port déjà occupé");
+			return -2;
+		}
+		
+		byte[] data = new byte[512];
+		DatagramPacket dp = new DatagramPacket(data, data.length, ip, port);
+		
+		try {
+			cli.send(dp);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 1;
+		}
+		
+		
+		byte[] buffer = new byte[512];															
+		DatagramPacket dp2 = new DatagramPacket(buffer, buffer.length);
+		try {
+			cli.receive(dp2);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+			return 1;
+		}
+	 
+	
+	return 0;
 	}
 }
